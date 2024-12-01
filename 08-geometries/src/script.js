20bf6c467268280c0baa52b8439c728f230fb081
@x0mg <jeff14994@gmail.com>
2024-12-01 14:37:20 -0700

    Complete Chapter01-08

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -11,8 +11,27 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// Create an empty BufferGeometry
+const geometry = new THREE.BufferGeometry()
+
+// Create 50 triangles (450 values)
+// 50 triangles -> each triangle has 3 vertices -> each vertic has 3 coordination (x, y, z)
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+for(let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+
+// Create the attribute and name it 'position'
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 40, 40, 40)
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
