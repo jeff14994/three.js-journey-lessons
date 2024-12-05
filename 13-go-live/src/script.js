cd5e4679fab074809351015f3a733d2dd065bc26
@x0mg <jeff14994@gmail.com>
2024-12-05 13:31:28 -0700

    Initialize Chapter02-14

--- 13-go-live/src/script.js
+++ 13-go-live/src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'WTF Three.js XDD',
             {
                 font: font,
                 size: 0.5,
