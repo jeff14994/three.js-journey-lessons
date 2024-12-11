85b6c9ae04c4ac2a008bbae2cf15a1557659516d
@x0mg <jeff14994@gmail.com>
2024-12-11 14:58:00 -0700

    Complete Chapter02-17

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -18,15 +18,58 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
+// Particles
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 5000
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+for(let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10.
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+console.log(particlesGeometry.attributes.position.array)
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true
+})
+// particlesMaterial.size = 0.1
+// particlesMaterial.color = new THREE.Color('#ff88cc')
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// remove the individual particle background
+particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+// particlesMaterial.blending = THREE.AdditiveBlending
+// To activate those vertex colors, simply change the vertexColors property to true:
+particlesMaterial.vertexColors = true
+
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
+//
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 /**
  * Sizes
@@ -71,7 +114,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+// axeshelper
+const axeshelper = new THREE.AxesHelper(5)
+scene.add(axeshelper)
 /**
  * Animate
  */
@@ -80,7 +125,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    // Update particles
+    // particles.rotation.y = - elapsedTime * 0.2
 
+    for(let i = 0; i < count; i++){
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
     // Update controls
     controls.update()
 
